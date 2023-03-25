ee9fa91ecbc7bb26993d005840c43db6a905f9ed
saurabh solanki <solankisourabh25@gmail.com>
2023-03-25 14:34:29 +0530

    back

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 require("dotenv").config();
 const mongo_url = process.env.mongo_url || "";
 
-mongoose.connect(mongo_url,{
+mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/paypal",{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then((res)=>{
